0ade89e23d78d945ea2ed6389cf00f0c083f44d2
Systemcluster <me@systemcluster.me>
2019-08-04 09:13:59 +0200

    Make textured intermediate step work

--- data/hlsl/fbm1.frag.hlsl
+++ data/hlsl/fbm1.frag.hlsl
@@ -11,7 +11,7 @@ struct Input {
 };
 
 struct Output {
-	float4 color : SV_TARGET0;
+	float value : SV_TARGET0;
 };
 
 #include "noise/util.hlsl"
@@ -99,73 +99,6 @@ Output main(Input input) {
 	hv = clamp(hv, 0.0, 1.0);
 
 
-	float3 output = float3(hv);
-	
-	if (mode == 1 || mode == 2) {
-		float3 c = output;
-		// abyss
-		if (hv <= 0.2) {
-			c = c * float3(0.3, 0.4, 0.9);
-		}
-		// deep water
-		else if (hv <= 0.4) {
-			c = c * float3(0.5, 0.6, 0.9);
-		}
-		// water
-		else if (hv <= 0.48) {
-			c = c * float3(0.5, 0.7, 1.0);
-		}
-		// shallow water
-		else if (hv <= 0.5) {
-			c = c * float3(0.6, 0.75, 1.0);
-		}
-		// coastline
-		else if (hv <= 0.52) {
-			c = c * float3(0.6, 0.9, 0.6);
-		}
-		// flatland
-		else if (hv <= 0.6) {
-			c = c * float3(0.7, 1.0, 0.7);
-		}
-		// highland
-		else if (hv <= 0.7) {
-			c = c * float3(0.9, 0.8, 0.4);
-		}
-		// mountain
-		else if (hv <= 0.8) {
-			c = c * float3(0.9, 0.6, 0.4);
-		}
-		// peak
-		else if (hv <= 1.0) {
-			c = c * float3(0.9, 0.4, 0.4);
-		}
-		output = c;
-	}
-
-	float zone = sin(position.y * PI2 * 3 + PI/2);
-	float pressure = (zone);
-	float direction = abs(sin(position.y * PI2 + PI/2)) * 2 - 1;
-	
-	float speed = direction * (pressure * 0.5 + 0.5);
-	
-	if (mode == 3) {
-		output = (float3(-1.0,  -1.0, speed) * 0.5 + 0.5);
-	}
-
-	if (mode == 4) {
-		output = (float3(pressure, -1.0,  -1.0) * 0.5 + 0.5);
-	}
-	if (mode == 5) {
-
-	}
-
-	if (mode == 6) {
-		output = float3(position.xy, 1);
-	}
-	// o.color = float4(hash(uint3(position.xy * 1000, 1.0)), 1.0);
-	// o.color = float4(position, 1.0, 1.0);
-	// o.color = float4(subregion.xyz, 1);
-	
-	o.color = float4(output, 1.0);
+	o.value = hv;
 	return o;
 }
--- src/renderer.rs
+++ src/renderer.rs
@@ -50,10 +50,14 @@ pub struct Renderer<'a> {
 	compiler: shaderc::Compiler,
 	options: shaderc::CompileOptions<'a>,
 
-	bind_group: wgpu::BindGroup,
+	bind_group_1: wgpu::BindGroup,
+	bind_group_2: wgpu::BindGroup,
+
     uniform_buf: wgpu::Buffer,
-	// texture_buf: wgpu::Texture,
-    pipeline: wgpu::RenderPipeline,
+	texture: wgpu::Texture,
+
+    pipeline_1: wgpu::RenderPipeline,
+    pipeline_2: wgpu::RenderPipeline,
 }
 impl<'a> Renderer<'a> {
 	pub fn init(swap_chain_descriptor: &wgpu::SwapChainDescriptor, device: &mut wgpu::Device) -> Result<Self, RendererError> {
@@ -67,7 +71,7 @@ impl<'a> Renderer<'a> {
 			options.set_optimization_level(shaderc::OptimizationLevel::Performance);
 		}
 		options.set_auto_bind_uniforms(true);
-		options.set_include_callback(|file, include_type, source, depth| {
+		options.set_include_callback(|file, _include_type, source, _depth| {
 			let mut path = std::env::current_dir().map_err(|e| e.to_string())?;
 			path.push(std::path::Path::new("data/hlsl/"));
 			path.push(std::path::Path::new(file));
@@ -81,104 +85,202 @@ impl<'a> Renderer<'a> {
 			})
 		});
 
-		let init_encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor { todo: 0});
+		let vs_shader = compile_shader(
+			&mut compiler, &options, std::path::Path::new("data/hlsl/fullscreen.vert.hlsl"), shaderc::ShaderKind::Vertex)?;
+		let fs_shader_1 = compile_shader(
+			&mut compiler, &options, std::path::Path::new("data/hlsl/fbm1.frag.hlsl"), shaderc::ShaderKind::Fragment)?;
+		let fs_shader_2 = compile_shader(
+			&mut compiler, &options, std::path::Path::new("data/hlsl/textured.frag.hlsl"), shaderc::ShaderKind::Fragment)?;
 
-		let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-			bindings: &[
-				wgpu::BindGroupLayoutBinding {
-					binding: 0,
-					visibility: wgpu::ShaderStage::FRAGMENT,
-					ty: wgpu::BindingType::UniformBuffer
-				}
-			]
-		});
+		let vs_module_1 = device.create_shader_module(vs_shader.as_binary_u8());
+		let vs_module_2 = device.create_shader_module(vs_shader.as_binary_u8());
+		let fs_module_1 = device.create_shader_module(fs_shader_1.as_binary_u8());
+		let fs_module_2 = device.create_shader_module(fs_shader_2.as_binary_u8());
 
-		let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-			bind_group_layouts: &[&bind_group_layout]
-		});
+		// let fs_reflect = spirv_reflect::ShaderModule::load_u8_data(fs_shader_1.as_binary_u8())?;
+		// let fs_bindings = fs_reflect.enumerate_descriptor_bindings(None)?;
+		// for ref input in fs_bindings.iter() {
+		// 	info!("{:#?}", input);
+		// }
 
 		let uniform_buf = device.create_buffer_mapped::<RendererArgs>(
 			1, 
 			wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::TRANSFER_DST
 		).finish();
 
-		let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-			layout: &bind_group_layout,
-			bindings: &[
-				wgpu::Binding {
-					binding: 0,
-					resource: wgpu::BindingResource::Buffer {
-						buffer: &uniform_buf,
-						range: 0..(std::mem::size_of::<RendererArgs>() as u64)
-					}
-				}
-			]
+		let texture = device.create_texture(&wgpu::TextureDescriptor {
+			size: wgpu::Extent3d {
+				width: 1000,
+				height: 1000,
+				depth: 1,
+			},
+			array_layer_count: 1,
+			mip_level_count: 1,
+			sample_count: 1,
+			dimension: wgpu::TextureDimension::D2,
+			format: wgpu::TextureFormat::R32Float,
+			usage: wgpu::TextureUsage::all()
 		});
-
-		let vs_shader = compile_shader(
-			&mut compiler, &options, std::path::Path::new("data/hlsl/fullscreen.vert.hlsl"), shaderc::ShaderKind::Vertex)?;
-		let fs_shader = compile_shader(
-			&mut compiler, &options, std::path::Path::new("data/hlsl/fbm1.frag.hlsl"), shaderc::ShaderKind::Fragment)?;
-
-		let vs_module = device.create_shader_module(vs_shader.as_binary_u8());
-		let fs_module = device.create_shader_module(fs_shader.as_binary_u8());
-
-		// let fs_reflect = spirv_reflect::ShaderModule::load_u8_data(fs_shader.as_binary_u8())?;
-		// let fs_bindings = fs_reflect.enumerate_descriptor_bindings(None)?;
-		// for ref input in fs_bindings.iter() {
-		// 	info!("{:#?}", input);
-		// }
-
-		// let texture_buf = device.create_texture(&wgpu::TextureDescriptor {
-		// 	size: wgpu::Extent3d {
-		// 		width: 1000,
-		// 		height: 1000,
-		// 		depth: 1,
-		// 	},
-		// 	array_layer_count: 1,
-		// 	mip_level_count: 1,
-		// 	sample_count: 1,
-		// 	dimension: wgpu::TextureDimension::D2,
-		// 	format: wgpu::TextureFormat::R32Float,
-		// 	usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT | wgpu::TextureUsage::TRANSFER_SRC
+		let texture_view = texture.create_default_view();
+		// let texture_buf = device.create_buffer(&wgpu::BufferDescriptor {
+		// 	size: ((1000 * 1000) * std::mem::size_of::<f32>()) as u64,
+		// 	usage: wgpu::BufferUsage::MAP_READ | wgpu::BufferUsage::TRANSFER_DST
 		// });
 
-		let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-			layout: &pipeline_layout,
-			vertex_stage: wgpu::PipelineStageDescriptor {
-				module: &vs_module,
-				entry_point: "main",
-			},
-			fragment_stage: Some(wgpu::PipelineStageDescriptor {
-				module: &fs_module,
-				entry_point: "main"
-			}),
-			rasterization_state: wgpu::RasterizationStateDescriptor {
-                front_face: wgpu::FrontFace::Ccw,
-                cull_mode: wgpu::CullMode::Back,
-                depth_bias: 0,
-                depth_bias_slope_scale: 0.0,
-                depth_bias_clamp: 0.0,
-            },
-            primitive_topology: wgpu::PrimitiveTopology::TriangleList,
-			color_states: &[wgpu::ColorStateDescriptor {
-                format: swap_chain_descriptor.format,
-                color_blend: wgpu::BlendDescriptor::REPLACE,
-                alpha_blend: wgpu::BlendDescriptor::REPLACE,
-                write_mask: wgpu::ColorWrite::ALL,
-            }],
-            depth_stencil_state: None,
-            index_format: wgpu::IndexFormat::Uint16,
-			vertex_buffers: &[],
-			sample_count: 1
-		});
-		let init_command_buffer = init_encoder.finish();
-		device.get_queue().submit(&[init_command_buffer]);
+
+		// let init_encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor { todo: 0 });
+
+		let (bind_group_1, pipeline_1) = {
+			let bind_group_layout_1 = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+				bindings: &[
+					wgpu::BindGroupLayoutBinding {
+						binding: 0,
+						visibility: wgpu::ShaderStage::FRAGMENT,
+						ty: wgpu::BindingType::UniformBuffer
+					}
+				]
+			});
+			let pipeline_layout_1 = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+				bind_group_layouts: &[&bind_group_layout_1]
+			});
+			let bind_group_1 = device.create_bind_group(&wgpu::BindGroupDescriptor {
+				layout: &bind_group_layout_1,
+				bindings: &[
+					wgpu::Binding {
+						binding: 0,
+						resource: wgpu::BindingResource::Buffer {
+							buffer: &uniform_buf,
+							range: 0..(std::mem::size_of::<RendererArgs>() as u64)
+						}
+					}
+				]
+			});
+			let pipeline_1 = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+				layout: &pipeline_layout_1,
+				vertex_stage: wgpu::PipelineStageDescriptor {
+					module: &vs_module_1,
+					entry_point: "main",
+				},
+				fragment_stage: Some(wgpu::PipelineStageDescriptor {
+					module: &fs_module_1,
+					entry_point: "main"
+				}),
+				rasterization_state: wgpu::RasterizationStateDescriptor {
+					front_face: wgpu::FrontFace::Ccw,
+					cull_mode: wgpu::CullMode::Back,
+					depth_bias: 0,
+					depth_bias_slope_scale: 0.0,
+					depth_bias_clamp: 0.0,
+				},
+				primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+				color_states: &[wgpu::ColorStateDescriptor {
+					format: wgpu::TextureFormat::R32Float,
+					color_blend: wgpu::BlendDescriptor::REPLACE,
+					alpha_blend: wgpu::BlendDescriptor::REPLACE,
+					write_mask: wgpu::ColorWrite::ALL,
+				}],
+				depth_stencil_state: None,
+				index_format: wgpu::IndexFormat::Uint16,
+				vertex_buffers: &[],
+				sample_count: 1
+			});
+			(bind_group_1, pipeline_1)
+		};
+
+		let (bind_group_2, pipeline_2) = {
+			let bind_group_layout_2 = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+				bindings: &[
+					wgpu::BindGroupLayoutBinding {
+						binding: 0,
+						visibility: wgpu::ShaderStage::FRAGMENT,
+						ty: wgpu::BindingType::UniformBuffer
+					},
+					wgpu::BindGroupLayoutBinding {
+						binding: 1,
+						visibility: wgpu::ShaderStage::FRAGMENT,
+						ty: wgpu::BindingType::SampledTexture
+					},
+					wgpu::BindGroupLayoutBinding {
+						binding: 2,
+						visibility: wgpu::ShaderStage::FRAGMENT,
+						ty: wgpu::BindingType::Sampler
+					}
+				]
+			});
+			let pipeline_layout_2 = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+				bind_group_layouts: &[&bind_group_layout_2]
+			});
+			let bind_group_2 = device.create_bind_group(&wgpu::BindGroupDescriptor {
+				layout: &bind_group_layout_2,
+				bindings: &[
+					wgpu::Binding {
+						binding: 0,
+						resource: wgpu::BindingResource::Buffer {
+							buffer: &uniform_buf,
+							range: 0..(std::mem::size_of::<RendererArgs>() as u64)
+						}
+					},
+					wgpu::Binding {
+						binding: 1,
+						resource: wgpu::BindingResource::TextureView(&texture_view)
+					},
+					wgpu::Binding {
+						binding: 2,
+						resource: wgpu::BindingResource::Sampler(&device.create_sampler(&wgpu::SamplerDescriptor {
+							address_mode_u: wgpu::AddressMode::ClampToEdge,
+							address_mode_v: wgpu::AddressMode::ClampToEdge,
+							address_mode_w: wgpu::AddressMode::ClampToEdge,
+							mag_filter: wgpu::FilterMode::Nearest,
+							min_filter: wgpu::FilterMode::Linear,
+							mipmap_filter: wgpu::FilterMode::Nearest,
+							lod_min_clamp: -100.0,
+							lod_max_clamp: 100.0,
+							compare_function: wgpu::CompareFunction::Always,
+						}))
+					},
+				]
+			});
+			let pipeline_2 = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+				layout: &pipeline_layout_2,
+				vertex_stage: wgpu::PipelineStageDescriptor {
+					module: &vs_module_2,
+					entry_point: "main",
+				},
+				fragment_stage: Some(wgpu::PipelineStageDescriptor {
+					module: &fs_module_2,
+					entry_point: "main"
+				}),
+				rasterization_state: wgpu::RasterizationStateDescriptor {
+					front_face: wgpu::FrontFace::Ccw,
+					cull_mode: wgpu::CullMode::Back,
+					depth_bias: 0,
+					depth_bias_slope_scale: 0.0,
+					depth_bias_clamp: 0.0,
+				},
+				primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+				color_states: &[wgpu::ColorStateDescriptor {
+					format: swap_chain_descriptor.format,
+					color_blend: wgpu::BlendDescriptor::REPLACE,
+					alpha_blend: wgpu::BlendDescriptor::REPLACE,
+					write_mask: wgpu::ColorWrite::ALL,
+				}],
+				depth_stencil_state: None,
+				index_format: wgpu::IndexFormat::Uint16,
+				vertex_buffers: &[],
+				sample_count: 1
+			});
+			(bind_group_2, pipeline_2)
+		};
+		
+		// let init_command_buffer = init_encoder.finish();
+		// device.get_queue().submit(&[init_command_buffer]);
 		Ok(Self {
-			bind_group,
+			bind_group_1,
+			bind_group_2,
 			uniform_buf,
-			// texture_buf,
-			pipeline,
+			texture,
+			pipeline_1,
+			pipeline_2,
 			compiler,
 			options
 		})
@@ -207,7 +309,24 @@ impl<'a> Renderer<'a> {
 
 		// render pass to texture
 		{
-
+			let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+				 color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                    attachment: &self.texture.create_default_view(),
+                    resolve_target: None,
+                    load_op: wgpu::LoadOp::Clear,
+                    store_op: wgpu::StoreOp::Store,
+                    clear_color: wgpu::Color {
+                        r: 0.1,
+                        g: 0.2,
+                        b: 0.3,
+                        a: 1.0,
+                    },
+                }],
+                depth_stencil_attachment: None,
+			});
+			render_pass.set_pipeline(&self.pipeline_1);
+			render_pass.set_bind_group(0, &self.bind_group_1, &[]);
+			render_pass.draw(0..6, 0..1);
 		}
 
 		// render pass to screen
@@ -227,8 +346,8 @@ impl<'a> Renderer<'a> {
                 }],
                 depth_stencil_attachment: None,
 			});
-			render_pass.set_pipeline(&self.pipeline);
-			render_pass.set_bind_group(0, &self.bind_group, &[]);
+			render_pass.set_pipeline(&self.pipeline_2);
+			render_pass.set_bind_group(0, &self.bind_group_2, &[]);
 			render_pass.draw(0..6, 0..1);
 		}
 		device.get_queue().submit(&[encoder.finish()]);
